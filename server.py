fa083dbf0cb3c7fdf986d40ebc8eeb50585b3b68
kebding <kebding@gmail.com>
2019-01-19 12:39:30 -0800

    save ids to .txt file, add page to show .txt file
    This commit saves every submission in a text file in the local
    directory. It also adds a button that links to a page that views the
    text file and has a button to return to the home page and clear the text
    file to allow for a fresh scanning session.

--- server.py
+++ server.py
@@ -6,17 +6,37 @@ import datetime
 import os
 from flask import g, session, abort, render_template, flash
 
-###################SERVER_ADDR = "http://35.197.98.244"
-
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 
 @app.route("/", methods = ['GET', 'POST'])
 def homepage():
-    return url_for('static', filename='index.html')
+    return app.send_static_file('index.html')
 
+@app.route("/add_id", methods=["POST"])
+def add_id():
+    if request.method != "POST":
+        return "bad request"
+    submission = str(request.form.get("id"))
+    ids_file = open("ids.txt", 'a')
+    ids_file.write(submission)
+    ids_file.write('\n')
+    ids_file.close()
+    return redirect('/', code=302)
 
+@app.route("/show_ids", methods = ["GET"])
+def show_ids():
+    with open("ids.txt", "r") as f:
+        content = f.read()
+    return render_template("submissions.html", content=content)
+
+@app.route("/clear", methods = ["POST"])
+def clear_ids():
+    ids_file = open("ids.txt", "w")
+    ids_file.close()
+    return redirect('/', code=302)
+    
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80) 
 
