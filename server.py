892437a1c21fe235bd1cd5b056bc1d9d94a5107a
Kyle <kebding@gmail.com>
2019-01-20 05:08:39 +0000

    POST section and week, add dropdown memory
    section and week selections now included in the POST message, which will
    help for adding entries to the database. the dropdowns now remain on
    their selections when the form is submitted, albeit with the cheap text
    file hack.

--- server.py
+++ server.py
@@ -13,43 +13,21 @@ app.config.from_object(__name__)
 
 @app.route("/", methods = ['GET', 'POST'])
 def homepage():
-    return redirect('/add_id', code=302)
+    return app.send_static_file('index.html')
 
-@app.route("/goto_scan", methods=["POST"])
-def goto_scan():
-    #ids_file = open("ids.txt", "w")
-    #ids_file.close()
-    content = ''
-    return render_template("scanner.html", content=content)
-
-@app.route("/add_id", methods=["GET", "POST"])
-def add_id():
-    submission = str(request.form.get("id"))
-    ids_file = open("ids.txt", 'a')
-    ids_file.write(submission)
-    ids_file.write('\n')
-    ids_file.close()  
-    try:
-        with open("ids.txt", "r") as f:
-            content = f.read()
-    except IOError:
-        # if there is no ids.txt, create an empty ids.txt
+@app.route("/scanner", methods=["GET", "POST"])
+def scanner():
+    if request.method == "POST":
+        content = get_current_settings()
+    else:
+        content = 'no submissions yet'
+        # clear ids.txt
         ids_file = open("ids.txt", "w")
         ids_file.close()
-        content = 'no submissions yet!'
-    return render_template("scanner.html", content=content)
-
-@app.route("/show_ids", methods = ["GET"])
-def show_ids():
-    try:
-        with open("ids.txt", "r") as f:
-            content = f.read()
-    except IOError:
-        # if there is no ids.txt, create an empty ids.txt
-        ids_file = open("ids.txt", "w")
-        ids_file.close()
-        content = ''
-    return render_template("submissions.html", content=content)
+    section = get_section()
+    week = get_week()
+    return render_template("scanner.html", content=content, 
+            current_section=section, current_week=week)
 
 @app.route("/save", methods = ["POST"])
 def save():
@@ -61,11 +39,67 @@ def save():
     c = db.cursor()
 
     c.execute('''CREATE TABLE IF NOT EXISTS attendances''')
-    new_entries = str(request.form.get("save"))
+    section_and_week= str(request.form.get("save_to_db"))
      
 
     return redirect('/', code=302)
     
+
+
+def get_section():
+    try:
+        with open("section.txt", "r") as f:
+            section = f.read().rstrip()
+        f.close()
+    except IOError:
+        section_file = open("section.txt", "w")
+        section_file.write("01A")
+        section_file.close()
+        section = "01A"
+    return section
+
+def get_week():
+    try:
+        with open("week.txt", "r") as f:
+            week = f.read().rstrip()
+        f.close()
+    except IOError:
+        week_file = open("week.txt", "w")
+        week_file.write("1")
+        week_file.close()
+        week = "1"
+    return week
+
+def get_current_settings():
+    #TODO: replace ids.txt with database 
+    submission = str(request.form.get("id_entry"))
+    if submission is not None and submission != "None":
+        ids_file = open("ids.txt", 'a')
+        ids_file.write(submission)
+        ids_file.write('\n')
+        ids_file.close()  
+    try:
+        with open("ids.txt", "r") as f:
+            content = f.read()
+        f.close()
+    except IOError:
+        # if there is no ids.txt, create an empty ids.txt
+        ids_file = open("ids.txt", "w")
+        ids_file.close()
+        content = 'no submissions yet'
+    week = str(request.form.get("week"))
+    if week is not None and week != "None":
+        week_file = open("week.txt", "w")
+        week_file.write(week)
+        week_file.close()
+    section = str(request.form.get("section"))
+    if section is not None and section != "None":
+        section_file = open("section.txt", "w")
+        section_file.write(section)
+        section_file.close()
+
+    return content
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80) 
 
