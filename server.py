c9e5a0fbb0d88018fb636ce8a1ad8da5f4d19b80
Kyle <kebding@gmail.com>
2019-01-20 01:01:34 +0000

    WIP: add database

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from subprocess import call
 import datetime 
 import os
 from flask import g, session, abort, render_template, flash
+import sqlite3
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -12,18 +13,25 @@ app.config.from_object(__name__)
 
 @app.route("/", methods = ['GET', 'POST'])
 def homepage():
-    return app.send_static_file('index.html')
+    return redirect('/add_id', code=302)
 
-@app.route("/add_id", methods=["POST"])
+@app.route("/add_id", methods=["GET", "POST"])
 def add_id():
-    if request.method != "POST":
-        return "bad request"
     submission = str(request.form.get("id"))
     ids_file = open("ids.txt", 'a')
     ids_file.write(submission)
     ids_file.write('\n')
-    ids_file.close()
-    return redirect('/', code=302)
+    ids_file.close()  
+    try:
+        with open("ids.txt", "r") as f:
+            content = f.read()
+    except IOError:
+        # if there is no ids.txt, create an empty ids.txt
+        ids_file = open("ids.txt", "w")
+        ids_file.close()
+        content = 'no submissions yet!'
+    #return redirect('/', code=302)
+    return render_template("index.html", content=content)
 
 @app.route("/show_ids", methods = ["GET"])
 def show_ids():
@@ -37,10 +45,19 @@ def show_ids():
         content = ''
     return render_template("submissions.html", content=content)
 
-@app.route("/clear", methods = ["POST"])
-def clear_ids():
-    ids_file = open("ids.txt", "w")
-    ids_file.close()
+@app.route("/save", methods = ["POST"])
+def save():
+    try:
+        db = sqlite3.connect('submissions.sqlite3')
+    except Error as e:
+        print(e)
+
+    c = db.cursor()
+
+    c.execute('''CREATE TABLE IF NOT EXISTS attendances''')
+    #new_entries = str(request.form.get("
+    # WIP 
+
     return redirect('/', code=302)
     
 if __name__ == '__main__':
